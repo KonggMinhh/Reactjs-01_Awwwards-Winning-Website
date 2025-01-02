864f3e5525ebc1069474894d51a401367bc10765
KonggMinhh <hoangkongminh2001@gmail.com>
2025-01-02 17:25:55 +0700

    first commit

---
+++ FE/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-4xl font-medium text-center'>Awwwards Winning Website</h1>
+    </main>
+  )
+}
+
+export default App
